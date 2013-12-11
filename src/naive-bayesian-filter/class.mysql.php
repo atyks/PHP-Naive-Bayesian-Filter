6e0b25bcd2e314b3f98283ea2277b8b3df5aa756
atyks <akio.yoshikawa@gmail.com>
2013-12-11 10:25:16 +0900

    change indent and line-break

--- src/naive-bayesian-filter/class.mysql.php
+++ src/naive-bayesian-filter/class.mysql.php
@@ -1,168 +1,165 @@
 <?php
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is DotClear Weblog.
-#
-# The Initial Developer of the Original Code is
-# Olivier Meunier.
-# Portions created by the Initial Developer are Copyright (C) 2003
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
+	# ***** BEGIN LICENSE BLOCK *****
+	# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+	#
+	# The contents of this file are subject to the Mozilla Public License Version
+	# 1.1 (the "License"); you may not use this file except in compliance with
+	# the License. You may obtain a copy of the License at
+	# http://www.mozilla.org/MPL/
+	#
+	# Software distributed under the License is distributed on an "AS IS" basis,
+	# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	# for the specific language governing rights and limitations under the
+	# License.
+	#
+	# The Original Code is DotClear Weblog.
+	#
+	# The Initial Developer of the Original Code is
+	# Olivier Meunier.
+	# Portions created by the Initial Developer are Copyright (C) 2003
+	# the Initial Developer. All Rights Reserved.
+	#
+	# Contributor(s):
+	#
+	# Alternatively, the contents of this file may be used under the terms of
+	# either the GNU General Public License Version 2 or later (the "GPL"), or
+	# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+	# in which case the provisions of the GPL or the LGPL are applicable instead
+	# of those above. If you wish to allow use of your version of this file only
+	# under the terms of either the GPL or the LGPL, and not to allow others to
+	# use your version of this file under the terms of the MPL, indicate your
+	# decision by deleting the provisions above and replace them with the notice
+	# and other provisions required by the GPL or the LGPL. If you do not delete
+	# the provisions above, a recipient may use your version of this file under
+	# the terms of any one of the MPL, the GPL or the LGPL.
+	#
+	# ***** END LICENSE BLOCK *****
 
-# Classe de connexion MySQL
+	# Classe de connexion MySQL
 
-require_once dirname(__FILE__).'/class.recordset.php';
+	require_once dirname(__FILE__) . '/class.recordset.php';
 
-class Connection
-{
-	var $con_id;
-	var $error;
-	var $errno;
-	
-	function Connection($user, $pwd , $alias='', $dbname)
-	{
-		$this->error = '';
-		
-		$this->con_id = @mysql_connect($alias, $user, $pwd);
-		
-		if (!$this->con_id) {
-			$this->setError();
-		} else {
-			$this->database($dbname);
-		}
-	}
-	
-	function database($dbname)
-	{
-		$db = @mysql_select_db($dbname);
-		if(!$db) {
-			$this->setError();
-		return false;
-		} else {
-			return true;
-		}
-	}
-	
-	function close()
-	{
-		if ($this->con_id) {
-			mysql_close($this->con_id);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	function select($query,$class='recordset')
-	{
-		if (!$this->con_id) {
-			return false;
-		}
-		
-		if ($class == '' || !class_exists($class)) {
-			$class = 'recordset';
-		}
-		
-		$cur = mysql_unbuffered_query($query, $this->con_id);
-		
-		if ($cur)
-		{
-			# Insertion dans le reccordset
-			$i = 0;
-			$arryRes = array();
-			while($res = mysql_fetch_row($cur))
-			{
-				for($j=0; $j<count($res); $j++)
-				{
-					$arryRes[$i][strtolower(mysql_field_name($cur, $j))] = $res[$j];		
-				}
-				$i++;
+	class Connection {
+		var $con_id;
+		var $error;
+		var $errno;
+
+		function Connection($user, $pwd, $alias = '', $dbname) {
+			$this->error = '';
+
+			$this->con_id = @mysql_connect($alias, $user, $pwd);
+
+			if (!$this->con_id) {
+				$this->setError();
 			}
-			return new $class($arryRes);
+			else {
+				$this->database($dbname);
+			}
 		}
-		else
-		{
-			$this->setError();
-			return false;
-		}
-	}
-	
-	function execute($query)
-	{
-		if (!$this->con_id) {
-			return false;
-		}
-		
-		$cur = mysql_query($query, $this->con_id);
-		
-		if (!$cur) {
-			$this->setError();
-			return false;
-		} else {
-			return true;
-		}
-		
-	}
-	
-	function getLastID()
-	{
-		if ($this->con_id) {
-			return mysql_insert_id($this->con_id);
-		} else {
-			return false;
-		}
-	}
-	
-	function setError()
-	{
-		if ($this->con_id) {
-			$this->error = mysql_error($this->con_id);
-			$this->errno = mysql_errno($this->con_id);
-		} else {
-			$this->error = mysql_error();
-			$this->errno = mysql_errno();
-		}
-	}
-	
-	function error()
-	{
-		if ($this->error != '') {
-			return $this->errno.' - '.$this->error;
-		} else {
-			return false;
-		}
-	}
-	
-	function escapeStr($str)
-	{
-		return mysql_escape_string($str);
-	}
-}
 
-?>
+		function database($dbname) {
+			$db = @mysql_select_db($dbname);
+			if (!$db) {
+				$this->setError();
+
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+
+		function close() {
+			if ($this->con_id) {
+				mysql_close($this->con_id);
+
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		function select($query, $class = 'recordset') {
+			if (!$this->con_id) {
+				return false;
+			}
+
+			if ($class == '' || !class_exists($class)) {
+				$class = 'recordset';
+			}
+
+			$cur = mysql_unbuffered_query($query, $this->con_id);
+
+			if ($cur) {
+				# Insertion dans le reccordset
+				$i = 0;
+				$arryRes = array();
+
+				while ($res = mysql_fetch_row($cur)) {
+					for ($j = 0; $j < count($res); $j++) {
+						$arryRes[$i][strtolower(mysql_field_name($cur, $j))] = $res[$j];
+					}
+					$i++;
+				}
+
+				return new $class($arryRes);
+			}
+			else {
+				$this->setError();
+
+				return false;
+			}
+		}
+
+		function execute($query) {
+			if (!$this->con_id) {
+				return false;
+			}
+
+			$cur = mysql_query($query, $this->con_id);
+
+			if (!$cur) {
+				$this->setError();
+
+				return false;
+			}
+			else {
+				return true;
+			}
+
+		}
+
+		function getLastID() {
+			if ($this->con_id) {
+				return mysql_insert_id($this->con_id);
+			}
+			else {
+				return false;
+			}
+		}
+
+		function setError() {
+			if ($this->con_id) {
+				$this->error = mysql_error($this->con_id);
+				$this->errno = mysql_errno($this->con_id);
+			}
+			else {
+				$this->error = mysql_error();
+				$this->errno = mysql_errno();
+			}
+		}
+
+		function error() {
+			if ($this->error != '') {
+				return $this->errno . ' - ' . $this->error;
+			}
+			else {
+				return false;
+			}
+		}
+
+		function escapeStr($str) {
+			return mysql_escape_string($str);
+		}
+	}
--- src/naive-bayesian-filter/class.naivebayesianstorage.php
+++ src/naive-bayesian-filter/class.naivebayesianstorage.php
@@ -1,221 +1,220 @@
 <?php
-/*
-  ***** BEGIN LICENSE BLOCK *****
-   This file is part of PHP Naive Bayesian Filter.
+	/*
+	 ***** BEGIN LICENSE BLOCK *****
+	 This file is part of PHP Naive Bayesian Filter.
 
-   The Initial Developer of the Original Code is
-   Loic d'Anterroches [loic_at_xhtml.net].
-   Portions created by the Initial Developer are Copyright (C) 2003
-   the Initial Developer. All Rights Reserved.
+	 The Initial Developer of the Original Code is
+	 Loic d'Anterroches [loic_at_xhtml.net].
+	 Portions created by the Initial Developer are Copyright (C) 2003
+	 the Initial Developer. All Rights Reserved.
 
-   Contributor(s):
+	 Contributor(s):
 
-   PHP Naive Bayesian Filter is free software; you can redistribute it
-   and/or modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
+	 PHP Naive Bayesian Filter is free software; you can redistribute it
+	 and/or modify it under the terms of the GNU General Public License as
+	 published by the Free Software Foundation; either version 2 of
+	 the License, or (at your option) any later version.
 
-   PHP Naive Bayesian Filter is distributed in the hope that it will
-   be useful, but WITHOUT ANY WARRANTY; without even the implied
-   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-   See the GNU General Public License for more details.
+	 PHP Naive Bayesian Filter is distributed in the hope that it will
+	 be useful, but WITHOUT ANY WARRANTY; without even the implied
+	 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	 See the GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with Foobar; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	 You should have received a copy of the GNU General Public License
+	 along with Foobar; if not, write to the Free Software
+	 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-   Alternatively, the contents of this file may be used under the terms of
-   the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-   in which case the provisions of the LGPL are applicable instead
-   of those above.
+	 Alternatively, the contents of this file may be used under the terms of
+	 the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+	 in which case the provisions of the LGPL are applicable instead
+	 of those above.
 
-  ***** END LICENSE BLOCK *****
-*/
+	 ***** END LICENSE BLOCK *****
+	 */
 
-/** Access to the storage of the data for the filter.
+	/** Access to the storage of the data for the filter.
 
-To avoid dependency with respect to any database, this class handle all the
-access to the data storage. You can provide your own class as long as
-all the methods are available. The current one rely on a MySQL database.
+	 To avoid dependency with respect to any database, this class handle all the
+	 access to the data storage. You can provide your own class as long as
+	 all the methods are available. The current one rely on a MySQL database.
 
-methods:
-    - array getCategories()
-    - bool  wordExists(string $word)
-    - array getWord(string $word, string $categoryid)
+	 methods:
+	 - array getCategories()
+	 - bool  wordExists(string $word)
+	 - array getWord(string $word, string $categoryid)
 
-*/
-class NaiveBayesianStorage
-{
-    var $con = null;
+	 */
+	class NaiveBayesianStorage {
+		var $con = null;
 
-    function NaiveBayesianStorage($user, $pwd , $server, $dbname)
-    {
-    	include_once dirname(__FILE__).'/class.mysql.php';
-    	$this->con = new Connection($user, $pwd , $server, $dbname);
-    	$this->con->execute('set names utf8');
-    	return true;
+		function NaiveBayesianStorage($user, $pwd, $server, $dbname) {
+			include_once dirname(__FILE__) . '/class.mysql.php';
 
-    }
+			$this->con = new Connection($user, $pwd, $server, $dbname);
+			$this->con->execute('set names utf8');
 
-    /** get the list of categories with basic data.
+			return true;
 
-        @return array key = category ids, values = array(keys = 'probability', 'word_count')
-    */
-    function getCategories()
-    {
-        $categories = array();
-        $rs = $this->con->select('SELECT * FROM nb_categories');
-        while (!$rs->EOF()) {
-            $categories[$rs->f('category_id')] = array('probability' => $rs->f('probability'),
-                                                       'word_count'  => $rs->f('word_count')
-                                                );
-            $rs->moveNext();
-        }
-        return $categories;
-    }
+		}
 
-    /** see if the word is an already learnt word.
-        @return bool
-        @param string word
-    */
-    function wordExists($word)
-    {
-        $rs = $this->con->select("SELECT * FROM nb_wordfreqs WHERE word='".$this->con->escapeStr($word)."'");
-        return !$rs->isEmpty();
-    }
+		/** get the list of categories with basic data.
 
-    /** get details of a word in a category.
-        @return array ('count' => count)
-        @param  string word
-        @param  string category id
-    */
-    function getWord($word, $category_id)
-    {
-        $details = array();
-        $rs = $this->con->select("SELECT * FROM nb_wordfreqs WHERE
-                                    word='".$this->con->escapeStr($word)."' AND
-                                    category_id='".$this->con->escapeStr($category_id)."'");
-        if ($rs->isEmpty()) $details['count'] = 0;
-        else $details['count'] = $rs->f('count');
-        return $details;
-    }
+		 @return array key = category ids, values = array(keys = 'probability', 'word_count')
+		 */
+		function getCategories() {
+			$categories = array();
+			$rs = $this->con->select('SELECT * FROM nb_categories');
 
-    /** update a word in a category.
-    If the word is new in this category it is added, else only the count is updated.
+			while (!$rs->EOF()) {
+				$categories[$rs->f('category_id')] = array('probability' => $rs->f('probability'),
+					'word_count'  => $rs->f('word_count')
+				);
 
-        @return bool success
-        @param string word
-        @param int    count
-        @paran string category id
-    */
-    function updateWord($word, $count, $category_id)
-    {
-    	$oldword = $this->getWord($word, $category_id);
-    	if (0 == $oldword['count']) {
-            return $this->con->execute("INSERT INTO nb_wordfreqs (word, category_id, count) VALUES
-                                ('".$this->con->escapeStr($word)."',
-                                 '".$this->con->escapeStr($category_id)."',
-                                 '".$this->con->escapeStr((int)$count)."')");
-        } else {
-            return $this->con->execute("UPDATE nb_wordfreqs SET count+=".(int)$count."
-                                        WHERE category_id = '".$this->con->escapeStr($category_id)."'
-                                        AND word = '".$this->con->escapeStr($word)."'");
-        }
-    }
+				$rs->moveNext();
+			}
 
-    /** remove a word from a category.
+			return $categories;
+		}
 
-        @return bool success
-        @param string word
-        @param int  count
-        @param string category id
-    */
-    function removeWord($word, $count, $category_id)
-    {
-    	$oldword = $this->getWord($word, $category_id);
-    	if (0 != $oldword['count'] && 0 >= ($oldword['count']-$count)) {
-            return $this->con->execute("DELETE FROM nb_wordfreqs WHERE
-                                word='".$this->con->escapeStr($word)."' AND
-                                category_id='".$this->con->escapeStr($category_id)."'");
-        } else {
-            return $this->con->execute("UPDATE nb_wordfreqs SET count-=".(int)$count."
-                                        WHERE category_id = '".$this->con->escapeStr($category_id)."'
-                                        AND word = '".$this->con->escapeStr($word)."'");
-        }
-    }
+		/** see if the word is an already learnt word.
+		 @return bool
+		 @param string word
+		 */
+		function wordExists($word) {
+			$rs = $this->con->select("SELECT * FROM nb_wordfreqs WHERE word='" . $this->con->escapeStr($word) . "'");
 
-    /** update the probabilities of the categories and word count.
-    This function must be run after a set of training
+			return !$rs->isEmpty();
+		}
 
-        @return bool sucess
-    */
-    function updateProbabilities()
-    {
-    	// first update the word count of each category
-        $rs = $this->con->select("SELECT category_id, SUM(count) AS total FROM nb_wordfreqs WHERE 1 GROUP BY category_id");
-        $total_words = 0;
-        while (!$rs->EOF()) {
-            $total_words += $rs->f('total');
-            $rs->moveNext();
-        }
-        $rs->moveStart();
-        if ($total_words == 0) {
-            $this->con->execute("UPDATE nb_categories SET word_count=0, probability=0 WHERE 1");
-            return true;
-        }
-        while (!$rs->EOF()) {
-            $proba = $rs->f('total')/$total_words;
-            $this->con->execute("UPDATE nb_categories SET word_count=".(int)$rs->f('total').",
-                                        probability=".$proba."
-                                        WHERE category_id = '".$rs->f('category_id')."'");
-            $rs->moveNext();
-        }
-        return true;
-    }
+		/** get details of a word in a category.
+		 @return array ('count' => count)
+		 @param  string word
+		 @param  string category id
+		 */
+		function getWord($word, $category_id) {
+			$details = array();
 
-    /** save a reference in the database.
+			$rs = $this->con->select("SELECT * FROM nb_wordfreqs WHERE word='" . $this->con->escapeStr($word) . "' AND category_id='" . $this->con->escapeStr($category_id) . "'");
 
-        @return bool success
-        @param  string reference if, must be unique
-        @param  string category id
-        @param  string content of the reference
-    */
-    function saveReference($doc_id, $category_id, $content)
-    {
-        return $this->con->execute("INSERT INTO nb_references (id, category_id, content) VALUES
-                                ('".$this->con->escapeStr($doc_id)."',
-                                 '".$this->con->escapeStr($category_id)."',
-                                 '".$this->con->escapeStr($content)."')");
-    }
+			if ($rs->isEmpty()) {
+				$details['count'] = 0;
+			}
+			else {
+				$details['count'] = $rs->f('count');
+			}
 
-    /** get a reference from the database.
+			return $details;
+		}
 
-        @return array  reference( category_id => ...., content => ....)
-        @param  string id
-    */
-    function getReference($doc_id)
-    {
-        $ref = array();
-        $rs = $this->con->select("SELECT * FROM nb_references WHERE id='".$this->con->escapeStr($doc_id)."'");
-        if ($rs->isEmpty()) return $ref;
-        $ref['category_id'] = $rs->f('category_id');
-        $ref['content'] = $rs->f('content');
-        $ref['id'] = $rs->f('id');
-        return $ref;
-    }
+		/** update a word in a category.
+		 If the word is new in this category it is added, else only the count is updated.
 
-    /** remove a reference from the database
+		 @return bool success
+		 @param string word
+		 @param int    count
+		 @paran string category id
+		 */
+		function updateWord($word, $count, $category_id) {
+			$oldword = $this->getWord($word, $category_id);
 
-        @return bool sucess
-        @param  string reference id
-    */
-    function removeReference($doc_id)
-    {
-        return $this->con->execute("DELETE FROM nb_references WHERE id='".$this->con->escapeStr($doc_id)."'");
-    }
+			if (0 == $oldword['count']) {
+				return $this->con->execute("INSERT INTO nb_wordfreqs (word, category_id, count) VALUES ('" . $this->con->escapeStr($word) . "', '" . $this->con->escapeStr($category_id) . "', '" . $this->con->escapeStr((int) $count) . "')");
+			}
+			else {
+				return $this->con->execute("UPDATE nb_wordfreqs SET count+=" . (int) $count . " WHERE category_id = '" . $this->con->escapeStr($category_id) . "' AND word = '" . $this->con->escapeStr($word) . "'");
+			}
+		}
 
+		/** remove a word from a category.
 
+		 @return bool success
+		 @param string word
+		 @param int  count
+		 @param string category id
+		 */
+		function removeWord($word, $count, $category_id) {
+			$oldword = $this->getWord($word, $category_id);
 
-}
+			if (0 != $oldword['count'] && 0 >= ($oldword['count'] - $count)) {
+				return $this->con->execute("DELETE FROM nb_wordfreqs WHERE word='" . $this->con->escapeStr($word) . "' AND category_id='" . $this->con->escapeStr($category_id) . "'");
+			}
+			else {
+				return $this->con->execute("UPDATE nb_wordfreqs SET count-=" . (int) $count . " WHERE category_id = '" . $this->con->escapeStr($category_id) . "' AND word = '" . $this->con->escapeStr($word) . "'");
+			}
+		}
 
-?>
+		/** update the probabilities of the categories and word count.
+		 This function must be run after a set of training
+
+		 @return bool sucess
+		 */
+		function updateProbabilities() {
+			// first update the word count of each category
+			$rs = $this->con->select("SELECT category_id, SUM(count) AS total FROM nb_wordfreqs WHERE 1 GROUP BY category_id");
+			$total_words = 0;
+
+			while (!$rs->EOF()) {
+				$total_words += $rs->f('total');
+				$rs->moveNext();
+			}
+
+			$rs->moveStart();
+
+			if ($total_words == 0) {
+				$this->con->execute("UPDATE nb_categories SET word_count=0, probability=0 WHERE 1");
+
+				return true;
+			}
+
+			while (!$rs->EOF()) {
+				$proba = $rs->f('total') / $total_words;
+				$this->con->execute("UPDATE nb_categories SET word_count=" . (int) $rs->f('total') . ", probability=" . $proba . " WHERE category_id = '" . $rs->f('category_id') . "'");
+				$rs->moveNext();
+			}
+
+			return true;
+		}
+
+		/** save a reference in the database.
+
+		 @return bool success
+		 @param  string reference if, must be unique
+		 @param  string category id
+		 @param  string content of the reference
+		 */
+		function saveReference($doc_id, $category_id, $content) {
+
+			return $this->con->execute("INSERT INTO nb_references (id, category_id, content) VALUES ('" . $this->con->escapeStr($doc_id) . "', '" . $this->con->escapeStr($category_id) . "', '" . $this->con->escapeStr($content) . "')");
+		}
+
+		/** get a reference from the database.
+
+		 @return array  reference( category_id => ...., content => ....)
+		 @param  string id
+		 */
+		function getReference($doc_id) {
+			$ref = array();
+			$rs = $this->con->select("SELECT * FROM nb_references WHERE id='" . $this->con->escapeStr($doc_id) . "'");
+
+			if ($rs->isEmpty()) {
+				return $ref;
+			}
+
+			$ref['category_id'] = $rs->f('category_id');
+			$ref['content'] = $rs->f('content');
+			$ref['id'] = $rs->f('id');
+
+			return $ref;
+		}
+
+		/** remove a reference from the database
+
+		 @return bool sucess
+		 @param  string reference id
+		 */
+		function removeReference($doc_id) {
+
+			return $this->con->execute("DELETE FROM nb_references WHERE id='" . $this->con->escapeStr($doc_id) . "'");
+		}
+
+	}
